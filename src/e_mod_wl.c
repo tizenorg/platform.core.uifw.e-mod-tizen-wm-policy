319c96aa40e6c331e25a152210779c7b413bbc1b
Mun, Gwan-gyeong <kk.moon@samsung.com>
2015-06-01 20:07:38 +0900

    Support 'notification level' protocol in wayland
    Change-Id: I839290b033551d00144f5cf23b9fc342e842021c

--- src/e_mod_wl.c
+++ src/e_mod_wl.c
@@ -1,7 +1,9 @@
 #include "e_mod_wl.h"
+#include "e_mod_notification.h"
 
 #include <wayland-server.h>
 #include "e_tizen_policy_server_protocol.h"
+#include "tizen_notification-server-protocol.h"
 
 typedef struct _Pol_Wayland
 {
@@ -9,7 +11,16 @@ typedef struct _Pol_Wayland
    Eina_List *visibility_list;
 } Pol_Wayland;
 
+typedef struct _Notification_Level
+{
+   int32_t level;
+   struct wl_resource *interface;
+   struct wl_resource *surface;
+} Notification_Level;
+
 static Eina_Hash *hash_pol_wayland = NULL;
+static Eina_Hash *hash_notification_levels = NULL;
+static Eina_Hash *hash_notification_interfaces = NULL;
 
 static Pol_Wayland*
 _pol_wayland_get_info (E_Pixmap *ep)
@@ -223,6 +234,97 @@ _e_tizen_policy_cb_bind(struct wl_client *client, void *data, uint32_t version, 
    wl_resource_set_implementation(res, &_e_tizen_policy_interface, cdata, NULL);
 }
 
+static void
+_e_tizen_notification_set_level_cb(struct wl_client   *client,
+                                   struct wl_resource *resource,
+                                   struct wl_resource *surface_resource,
+                                   uint32_t            level)
+{
+   E_Pixmap *ep;
+   E_Client *ec;
+
+   /* get the pixmap from this surface so we can find the client */
+   if (!(ep = wl_resource_get_user_data(surface_resource)))
+     {
+        wl_resource_post_error(surface_resource,
+                               WL_DISPLAY_ERROR_INVALID_OBJECT,
+                               "No Pixmap Set On Surface");
+        return;
+     }
+
+   /* make sure it's a wayland pixmap */
+   if (e_pixmap_type_get(ep) != E_PIXMAP_TYPE_WL) return;
+
+   /* find the client for this pixmap */
+   ec = e_pixmap_client_get(ep);
+   if (ec)
+     {
+        e_mod_pol_notification_level_apply(ec, level);
+
+        //Add other error handling code on notification send done.
+        tizen_notification_send_done(resource, surface_resource, level, TIZEN_NOTIFICATION_ERROR_STATE_NONE);
+     }
+   else
+     {
+         Notification_Level *nl;
+         nl = eina_hash_find(hash_notification_levels, &surface_resource);
+         if (!nl)
+           {
+              nl = E_NEW(Notification_Level, 1);
+              EINA_SAFETY_ON_NULL_RETURN(nl);
+              eina_hash_add(hash_notification_levels, &surface_resource, nl);
+           }
+
+         nl->level = level;
+         nl->interface = resource;
+         nl->surface = surface_resource;
+     }
+}
+
+static const struct tizen_notification_interface _e_tizen_notification_interface =
+{
+   _e_tizen_notification_set_level_cb
+};
+
+static void
+_e_tizen_notification_destroy(struct wl_resource *resource)
+{
+   struct wl_resource *noti_interface = NULL;
+
+   if (!resource) return;
+
+   noti_interface = eina_hash_find(hash_notification_interfaces, &resource);
+   if (!noti_interface) return;
+
+   eina_hash_del_by_key(hash_notification_interfaces, &resource);
+}
+
+static void
+_e_tizen_notification_bind_cb(struct wl_client *client, void *data, uint32_t version, uint32_t id)
+{
+   E_Comp_Data *cdata;
+   struct wl_resource *res;
+   struct wl_resource *noti_interface;
+
+   if (!(cdata = data))
+     {
+        wl_client_post_no_memory(client);
+        return;
+     }
+
+   if (!(res = wl_resource_create(client, &tizen_notification_interface, version, id)))
+     {
+        ERR("Could not create tizen_notification resource: %m");
+        wl_client_post_no_memory(client);
+        return;
+     }
+   wl_resource_set_implementation(res, &_e_tizen_notification_interface, cdata, _e_tizen_notification_destroy);
+
+   noti_interface = eina_hash_find(hash_notification_interfaces, &res);
+   if (!noti_interface)
+     eina_hash_add(hash_notification_interfaces, &res, res);
+}
+
 Eina_Bool
 e_mod_pol_wl_init(void)
 {
@@ -241,7 +343,16 @@ e_mod_pol_wl_init(void)
         return EINA_FALSE;
      }
 
+  if (!wl_global_create(cdata->wl.disp, &tizen_notification_interface, 1,
+                        cdata, _e_tizen_notification_bind_cb))
+    {
+       ERR("Could not add tizen_notification to wayland globals: %m");
+       return EINA_FALSE;
+    }
+
    hash_pol_wayland = eina_hash_pointer_new(free);
+   hash_notification_levels = eina_hash_pointer_new(free);
+   hash_notification_interfaces = eina_hash_pointer_new(NULL); // do not free by hash_del by key.
 
    return EINA_TRUE;
 }
@@ -250,6 +361,8 @@ void
 e_mod_pol_wl_shutdown(void)
 {
    E_FREE_FUNC(hash_pol_wayland, eina_hash_free);
+   E_FREE_FUNC(hash_notification_levels, eina_hash_free);
+   E_FREE_FUNC(hash_notification_interfaces, eina_hash_free);
 }
 
 void
@@ -285,5 +398,38 @@ e_mod_pol_wl_visibility_send(E_Client *ec, int visibility)
    EINA_LIST_FOREACH(pn->visibility_list, l, resource)
      {
         tizen_visibility_send_notify(resource, visibility);
+     }
+}
+
+void
+e_mod_pol_wl_notification_level_fetch(E_Client *ec)
+{
+   Notification_Level *nl;
+   E_Comp_Client_Data *cdata;
+   struct wl_resource *surface;
+   struct wl_resource *noti_interface;
+
+   EINA_SAFETY_ON_NULL_RETURN(ec);
+   cdata = e_pixmap_cdata_get(ec->pixmap);
+   EINA_SAFETY_ON_NULL_RETURN(cdata);
+   surface = cdata->wl_surface;
+   EINA_SAFETY_ON_NULL_RETURN(surface);
+
+   nl = eina_hash_find(hash_notification_levels, &surface);
+   if (nl)
+     {
+        noti_interface = eina_hash_find(hash_notification_interfaces, &(nl->interface));
+        if (noti_interface)
+          {
+             e_mod_pol_notification_level_apply(ec, nl->level);
+
+             //Add other error handling code on notification send done.
+             tizen_notification_send_done(nl->interface,
+                                          nl->surface,
+                                          nl->level,
+                                          TIZEN_NOTIFICATION_ERROR_STATE_NONE);
+          }
+
+        eina_hash_del_by_key(hash_notification_levels, &surface);
      }
 }
