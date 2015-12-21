a6025863124ad8d70ccc5011bfc6e8c76e85bb99
Doyoun Kang <doyoun.kang@samsung.com>
2015-12-21 13:59:00 +0900

    e_mod_wl: fixed bug for activating/uniconifying window - The application window was not updated because the internal iconic state didn't have changed when the application requested to activate/uniconify.
    Change-Id: I7181804829e8b3132545069395bd02f9c53600c9

--- src/e_mod_wl.c
+++ src/e_mod_wl.c
@@ -979,7 +979,11 @@ _tzpol_iface_cb_activate(struct wl_client *client EINA_UNUSED, struct wl_resourc
    EINA_SAFETY_ON_NULL_RETURN(ec->frame);
 
    if ((!starting) && (!ec->focused))
-     e_client_activate(ec, EINA_TRUE);
+     {
+        if ((ec->iconic) && (!ec->exp_iconify.by_client))
+          e_mod_pol_wl_iconify_state_change_send(ec, 0);
+        e_client_activate(ec, EINA_TRUE);
+     }
    else
      evas_object_raise(ec->frame);
 }
@@ -1617,6 +1621,9 @@ _tzpol_iface_cb_uniconify(struct wl_client *client EINA_UNUSED, struct wl_resour
    ec = e_pixmap_client_get(cp);
    EINA_SAFETY_ON_NULL_RETURN(ec);
    EINA_SAFETY_ON_NULL_RETURN(ec->frame);
+
+   if ((ec->iconic) && (!ec->exp_iconify.by_client))
+     e_mod_pol_wl_iconify_state_change_send(ec, 0);
 
    ELOG("Un-Set ICONIFY BY CLIENT", ec->pixmap, ec);
    ec->exp_iconify.by_client = 0;
