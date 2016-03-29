7973d5513729d4da0e357398eef86af351bf1429
Doyoun Kang <doyoun.kang@samsung.com>
2016-03-29 16:44:35 +0900

    set skip_iconify value to the keyboard's sub window such as magnifier
    Change-Id: Ic8d2a0e1337766fbf26054576b0d9e8ee607e1d3

--- src/e_mod_main.c
+++ src/e_mod_main.c
@@ -332,6 +332,8 @@ _pol_cb_hook_client_eval_pre_new_client(void *d EINA_UNUSED, E_Client *ec)
    if (e_mod_pol_client_is_keyboard_sub(ec))
      {
         ec->placed = 1;
+        ec->exp_iconify.skip_iconify = EINA_TRUE;
+
         EINA_SAFETY_ON_NULL_RETURN(ec->frame);
         if (ec->layer != E_LAYER_CLIENT_ABOVE)
           evas_object_layer_set(ec->frame, E_LAYER_CLIENT_ABOVE);
