f2486ffc905510a8cc076594d9c5c5580190af2e
Doyoun Kang <doyoun.kang@samsung.com>
2016-03-31 13:08:03 +0900

    aux hint: check whether the aux hint is changed or not before handling aux hint values.
    Change-Id: I4f9e9648e272406c2f5b4c0f1d126c5c06f61d05

--- src/e_mod_wl.c
+++ src/e_mod_wl.c
@@ -1851,6 +1851,11 @@ e_mod_pol_wl_eval_pre_post_fetch(E_Client *ec)
    Eina_List *l;
    Eina_Bool send;
 
+#ifdef HAVE_WAYLAND_ONLY
+   E_Comp_Wl_Client_Data *cdata = (E_Comp_Wl_Client_Data*)ec->comp_data;
+   if (cdata && !cdata->aux_hint.changed) return;
+#endif
+
    EINA_LIST_FOREACH(ec->comp_data->aux_hint.hints, l, hint)
      {
         send = EINA_FALSE;
